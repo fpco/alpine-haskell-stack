8c43a1f1b9f55f6b08c5cbc0e9a6ef5b5028fbf7
Sibi Prabakaran <sibi@psibi.in>
2023-03-04 16:22:18 +0530

    Support for GHC 9.2.7

--- ghc-musl.nix
+++ ghc-musl.nix
@@ -1,25 +1,30 @@
-{ pkgs ? import (builtins.fetchTarball {
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/01ce03efa4c8e8592632501a362de69098e91f4a.tar.gz";
-}) { } }:
+{ pkgs ? import
+    (builtins.fetchTarball {
+      url =
+        "https://github.com/NixOS/nixpkgs/archive/81d5cb1550ef0a58c5ee529c743065cc93a7fb64.tar.gz";
+    })
+    { }
+}:
 let
   alpineImage = pkgs.dockerTools.pullImage {
     imageName = "alpine";
     imageDigest =
-      "sha256:4ff3ca91275773af45cb4b0834e12b7eb47d1c18f770a0b151381cd227f4c253";
-    sha256 = "sha256:0p3jd744ndyfpz0snivx1v5nc3z7439whgg6farccgm8c3a5pjp2";
+      "sha256:e2e16842c9b54d985bf1ef9242a313f36b856181f188de21313820e177002501";
+    sha256 = "sha256-YCYpUByIOig4zt/WIvDas3AwHjDYkIQJ6IEKuADJmgg=";
   };
-in pkgs.dockerTools.buildImage {
+in
+pkgs.dockerTools.buildImage {
   name = "psibi/alpine-haskell-stack";
   tag = "v3";
 
   fromImage = alpineImage;
 
-  contents = [ pkgs.pkgsMusl.haskell.compiler.ghc925
-               pkgs.pkgsMusl.zlib
-               pkgs.pkgsMusl.zlib.dev
-               pkgs.pkgsMusl.ncurses
-             ];
+  contents = [
+    pkgs.pkgsMusl.haskell.compiler.ghc927
+    pkgs.pkgsMusl.zlib
+    pkgs.pkgsMusl.zlib.dev
+    pkgs.pkgsMusl.ncurses
+  ];
 
   config = { Cmd = [ "/bin/sh" ]; };
 }
--- justfile
+++ justfile
@@ -1,14 +1,16 @@
+GHC_VERSION := "9.2.7"
+
 # List all recipies
 default:
     just --list --unsorted
 
 # Build docker image
 build-image:
-    docker image build . -f Dockerfile -t fpco/alpine-haskell-stack:9.2.5
+    docker image build . -f Dockerfile -t fpco/alpine-haskell-stack:{{GHC_VERSION}}
 
 # Push image
 push-image:
-    docker push fpco/alpine-haskell-stack:9.2.5
+    docker push fpco/alpine-haskell-stack:{{GHC_VERSION}}
 
 # Build nix image
 build-nix-image:
@@ -17,3 +19,7 @@ build-nix-image:
 # Load nix image
 load-nix-image:
     docker load < result
+
+# Test image
+test-image:
+    docker run --tty --interactive fpco/alpine-haskell-stack:{{GHC_VERSION}} sh
